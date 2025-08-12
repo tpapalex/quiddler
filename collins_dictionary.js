63b5365cd8f80d3c944642e77be10a69cf3044e4
Ted <tpapalex@mit.edu>
2025-08-13 02:19:54 +0300

    add large files

---
+++ collins_dictionary.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:dd0a7b506366f280eec855a5507212f9c668a68d4864fc02dd2431436c6f8bff
+size 20597133
---
+++ common_lemmas.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:98f019bb61f9d10e87197c6bbbe91af22960f994229ffe6a20ec8c9ac0887023
+size 1172445
